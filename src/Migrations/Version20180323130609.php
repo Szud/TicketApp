a2febef5bab3c0e17df4ea3f29ca6f0840af79ad
ClementVallat <clementvallatpro@gmail.com>
2018-03-23 13:06:47 +0000

    migration bbd

---
+++ src/Migrations/Version20180323130609.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180323130609 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE app_users (id INTEGER NOT NULL, username VARCHAR(25) NOT NULL, password VARCHAR(255) NOT NULL, is_active BOOLEAN NOT NULL, roles CLOB NOT NULL --(DC2Type:json_array)
+        , PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_C2502824F85E0677 ON app_users (username)');
+        $this->addSql('CREATE TABLE test (id INTEGER NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE ticket (id INTEGER NOT NULL, poste CLOB NOT NULL, qualification CLOB NOT NULL, description CLOB NOT NULL, typeprobleme CLOB NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE app_users');
+        $this->addSql('DROP TABLE test');
+        $this->addSql('DROP TABLE ticket');
+    }
+}
