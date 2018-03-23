df55d1e9786d7518a5a5347a835d954211808c01
Szud <aszkudlarck@gmail.com>
2018-03-23 12:59:42 +0000

    commit user final

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -35,7 +35,7 @@ class User implements UserInterface
 
     private $plainPassword; 
     /**
-    * @ORM\Column(name="roles", type="json_arrays")
+    * @ORM\Column(name="roles", type="json_array")
     **/
     private $roles;
 
