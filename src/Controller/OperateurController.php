3712ebfe059aecc6da1e68cca8553484ede3bc7c
marcymark <marcjeanroy74@gmail.com>
2018-03-23 13:31:45 +0000

    menu

---
+++ src/Controller/OperateurController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route; //add this line to add usage of Route class
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * @Route("/operateur")
+ */ 
+
+class OperateurController extends Controller
+{
+
+    /**
+     * @Route("/home", name="app_home_operateur") //add this comment to annotations
+     */
+    public function homeAction()
+    {
+
+        return $this->render("Operateur/homeOperateur.html.twig");
+        
+    }
+
+
+    /**
+     * @Route("/tickets", name="app_consulterTicket_operateur") //add this comment to annotations
+     */
+    public function consulterTickets(Request $request)
+    {
+            
+    }
+
+
+}
